b437695138d34262bf991347657a069da05eec50
ank1331 <ankitsharma133189@gmail.com>
2023-10-30 22:54:35 +0530

    completing the UI for Signup Page

--- api/src/Components/SignIn.jsx
+++ api/src/Components/SignIn.jsx
@@ -5,18 +5,18 @@ import { AiFillApple } from "react-icons/ai";
 export default function SignIn() {
   return (
     <div>
-      this is sign in page
-      <div className=" flex justify-center ">
+      
+      <div className=" flex justify-between ">
         <img
-          className="mt-40 w-[30%] "
+          className="mt-40 ml-40 w-[25%] h-[60%] "
           src="https://freelogopng.com/images/all_img/1692007265x-twitter-logo-png.png"
         />
-        <div className="w-1/2 pl-20 mt-20 ">
+        <div className="w-1/2 pl-20  mt-20 ">
           <h1 className=" text-6xl pr-12  font-bold"> Happening now</h1>
           <h3 className=" text-2xl pr-12 mt-12 font-bold"> Join today.</h3>
           <div className="flex flex-col  mx-auto">
             <div className="  relative items-center ">
-              <button className=" absoulute top-2 border-2 px-4 h-[40px] mt-4 w-[250px] text-center rounded-full border-none ring-2 ring-slate-200">
+              <button className=" absoulute top-2 px-4 h-[40px] mt-4 w-[300px] text-center rounded-full border-none ring-2 ring-slate-200 hover:bg-slate-100">
                 {" "}
                 Signup with Google
               </button>
@@ -24,7 +24,7 @@ export default function SignIn() {
             </div>
 
             <div className="  relative items-center ">
-              <button className=" absoulute top-2 border-2 px-4 h-[40px] mt-4 w-[250px] text-center rounded-full border-none ring-2 ring-slate-200">
+              <button className=" absoulute top-2 border-2 px-4 h-[40px] mt-4 w-[300px] text-center rounded-full border-none ring-2 ring-slate-200 hover:bg-slate-200 font-bold">
                 {" "}
                 Signup with Apple
               </button>
@@ -37,14 +37,14 @@ export default function SignIn() {
             <div class="px-4">or</div>
             <div class="border-t border-gray-300 flex-1"></div>
           </div>
-          <button> Create Account </button>
-          <p>
-            By signing up, you agree to the Terms of Service and Privacy Policy,
-            including Cookie Use.
+          <button className="border-2 px-4 h-[40px] mt-4 w-[300px] text-center rounded-full border-none bg-sky-500 font-bold text-sm text-white ring-slate-200 hover:brightness-95 transition duration-10 ease-in-out "> Create Account </button>
+          <p className="mt-4 text-xs w-[300px]">
+            By signing up, you agree to the <span className="text-sky-500 hover:underline"> Terms of Service</span> and <span className="text-sky-500 hover:underline">Privacy Policy</span>,
+            including <span className="text-sky-500 hover:underline">Cookie Use.</span>
           </p>
 
-          <h4>Already have an account?</h4>
-          <button>Sign Up</button>
+          <h4 className=" font-bold text-lg mt-16">Already have an account?</h4>
+          <button className="border-2 mb-10 px-4 h-[40px] mt-4 w-[300px] text-center rounded-full text-sky-500 font-bold text-sm ring-1 ring-slate-200 hover:bg-sky-200">Sign In</button>
         </div>
       </div>
     </div>
